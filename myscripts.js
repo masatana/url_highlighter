8e0764e891a738274534c5a97fd523b60b45ff03
masatana <plaza.tumbling@gmail.com>
2013-01-31 22:51:52 +0900

    Finish alpha version
    Finish the coding alpha version for now.
    There is a SERIOUS BUG in this version.
    If there is an "EOF" at the end of the URL, all of the URLs' color
    changes.
    Maybe I can fix it by normalizing the "url_lists" in "myscripts.js"

--- myscripts.js
+++ myscripts.js
@@ -1,37 +1,32 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// matome sites urls
-//
-//
-var array_matome_sites = []
+var url_lists = [];
 var storage = chrome.storage.local;
 storage.get("urls", function(items) {
     console.log(items);
     if (items.urls) {
-        alert(items.urls);
+        url_lists = (items.urls).split("\n");
+        alert(url_lists);
     }
-});
-        
-/*
-$(function() {
-    alert();
-    var i;
-    $(this).getElementsByTagName("a").each(function() {
-        alert();
-    });
-    alert(i);
-});
-*/
-$(document).ready(function() {
     links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
-        for (var j = 0; j < array_matome_sites.length; j++) {
-            if (links[i].toString().indexOf(array_matome_sites[j]) !== -1) {
+        for (var j = 0; j < url_lists.length; j++) {
+            if (links[i].toString().indexOf(url_lists[j]) !== -1) {
+                links[i].style.color = "#0F0";
+            }
+        }
+    }
+});
+/*
+alert("test");
+$(document).ready(function() {
+    alert("test");
+    links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        for (var j = 0; j < url_lists.length; j++) {
+            if (links[i].toString().indexOf(url_lists[j]) !== -1) {
                 links[i].style.color = "#0F0";
                 alert();
             }
         }
     }
 });
+*/
