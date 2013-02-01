ec9f4539130b736be5b780aee72610117e057c5a
masatana <plaza.tumbling@gmail.com>
2013-02-01 23:05:35 +0900

    Using the jquery.highlight.js
    It can highlight more beautiful than ever.
    
    Still there is a bug. This extension's option required not to write EOF
    the end of the URL.

---
+++ jquery.highlight.js
@@ -0,0 +1,45 @@
+/*
+ * MIT license.
+ * This is the fork of "jquery.highlight-4.js"
+ * see <http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html>
+ */
+jQuery.fn.highlight = function(pat, url) {
+    function innerHighlight(node, pat) {
+        var skip = 0;
+        if (node.nodeType == 3) {
+            var pos = node.data.toUpperCase().indexOf(pat);
+            if (pos >= 0) {
+                var spannode = document.createElement('span');
+                spannode.className = 'highlight';
+                var middlebit = node.splitText(pos);
+                var endbit = middlebit.splitText(pat.length);
+                var middleclone = middlebit.cloneNode(true);
+                spannode.appendChild(middleclone);
+                middlebit.parentNode.replaceChild(spannode, middlebit);
+                skip = 1;
+            }
+        }
+        else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
+            for (var i = 0; i < node.childNodes.length; ++i) {
+                // Make sure that the URL is the same
+                if (node.toString() == url) {
+                    i += innerHighlight(node.childNodes[i], pat);
+                }
+            }
+        }
+        return skip;
+    }
+    return this.length && pat && pat.length ? this.each(function() {
+        innerHighlight(this, pat.toUpperCase());
+    }) : this;
+};
+
+jQuery.fn.removeHighlight = function() {
+    return this.find("span.highlight").each(function() {
+        this.parentNode.firstChild.nodeName;
+        with (this.parentNode) {
+            replaceChild(this.firstChild, this);
+            normalize();
+        }
+    }).end();
+};
--- myscripts.js
+++ myscripts.js
@@ -1,32 +1,25 @@
+/*
+ * MIT license.
+ */
 var url_lists = [];
 var storage = chrome.storage.local;
+
 storage.get("urls", function(items) {
     console.log(items);
     if (items.urls) {
-        url_lists = (items.urls).split("\n");
-        alert(url_lists);
+        url_lists = (items.urls).split("¥n");
     }
+    /*
+    if (url_lists[url_lists.length-1] == "¥n") {
+        url_lists.pop();
+    }
+    */
     links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
         for (var j = 0; j < url_lists.length; j++) {
             if (links[i].toString().indexOf(url_lists[j]) !== -1) {
-                links[i].style.color = "#0F0";
+                $("a").highlight(links[i].firstChild.nodeValue, links[i].toString());
             }
         }
     }
 });
-/*
-alert("test");
-$(document).ready(function() {
-    alert("test");
-    links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        for (var j = 0; j < url_lists.length; j++) {
-            if (links[i].toString().indexOf(url_lists[j]) !== -1) {
-                links[i].style.color = "#0F0";
-                alert();
-            }
-        }
-    }
-});
-*/
